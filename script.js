7fa92424634b812b607de3e85bee853eb1552150
yxCarla <70400085+yxCarla@users.noreply.github.com>
2020-10-27 20:32:26 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,108 @@
-var tempOutputCelsius = document.getElementById('celsius_output');
-var tempOutputFahrenheit = document.getElementById('fahrenheit_output');
-var tempInput = document.getElementById('temp_input');
-var convertCelsiusButton = document.getElementById('convert_celsius');
-var convertFahrenheitButton = document.getElementById('convert_fahrenheit')
-
-var fahrenheitBox = document.getElementById('fahrenheit_box');
-var celsiusBox = document.getElementById('celsius_box');
-
-var chooseCButton = document.getElementById('chooseC');
-var chooseFButton = document.getElementById('chooseF');
-
-var mainBox = document.getElementById('main_box');
+var outputs = document.getElementById('outputs');
 
 function hideAll() {
-    mainBox.style.display = 'none';
+    chosen_f.style.display = 'none';
+    chosen_c.style.display = 'none';
+    chosen_k.style.display = 'none';
+    outputs.style.display = 'none';
 }
 
-function convertC() {
-    var celsiusOutput = tempInput.value;
-    var celsiusOutput = ((tempInput.value) - 32) / 1.8;
+var chosen_f = document.getElementById('f_starting');
+var chosen_c = document.getElementById('c_starting');
+var chosen_k = document.getElementById('k_starting');
 
-    tempOutputCelsius.innerHTML = celsiusOutput.toFixed(1);
+
+function chosenF() {
+    chosen_f.style.display = 'block';
+    chosen_c.style.display = 'none';
+    chosen_k.style.display = 'none';
+    outputs.style.display = 'block';
+    
+
+    footerInfo.innerHTML = "You are converting from Fahrenheit";
 }
 
-convertCelsiusButton.addEventListener('click', convertC);
-
-function convertF() {
-    var fahrenheitOutput = tempInput.value;
-    var fahrenheitOutput = ((tempInput.value) * 1.8) + 32;
-
-    tempOutputFahrenheit.innerHTML = fahrenheitOutput.toFixed(1);
+function chosenC() {
+    chosen_c.style.display = 'block';
+    chosen_f.style.display = 'none';
+    chosen_k.style.display = 'none';
+    outputs.style.display = 'block';
+    
+    footerInfo.innerHTML = "You are converting from Celsius";
 }
 
-convertFahrenheitButton.addEventListener('click', convertF);
-
-function chooseC() {
-    fahrenheitBox.style.display = 'none';
-    celsiusBox.style.display = 'block';
-    mainBox.style.display = 'block';
-    chooseCButton.style.border = '0.2vw solid black';
-    chooseFButton.style.border = 'none';
+function chosenK() {
+    chosen_f.style.display = 'none'
+    chosen_c.style.display = 'none';
+    chosen_k.style.display = 'block';
+    outputs.style.display = 'block';
+    
+    footerInfo.innerHTML = "You are converting from Kelvin";
 }
 
-chooseCButton.addEventListener('click', chooseC);
 
-function chooseF() {
-    celsiusBox.style.display = 'none';
-    fahrenheitBox.style.display = 'block';
-    mainBox.style.display = 'block';
-    chooseFButton.style.border = '0.2vw solid black';
-    chooseCButton.style.border = 'none';
+
+var fOutput = document.getElementById('f_output');
+var cOutput = document.getElementById('c_output');
+var kOutput = document.getElementById('k_output');
+
+
+
+function convertNoF() {
+    var f_output = f_starting.value;
+    var f_output = ((f_starting.value) * 1);
+
+    fOutput.innerHTML = 'Fahrenheit:' + " " + f_output.toFixed(2);
+    var c_output = f_starting.value;
+    var c_output = (((f_starting.value) - 32 ) / 1.8);
+
+    cOutput.innerHTML = 'Celsius:' + " " + c_output.toFixed(2);
+
+    var k_output = f_starting.value;
+    var k_output = (((f_starting.value) - 32 ) / 1.8 + 273.15);
+
+    kOutput.innerHTML = 'Kelvin:' + " " + k_output.toFixed(2);
+   
 }
 
-chooseFButton.addEventListener('click', chooseF);
+function convertNoC() {
+    var f_output = c_starting.value;
+    var f_output = ((c_starting.value) * 1.8 + 32);
+
+    fOutput.innerHTML = 'Fahrenheit:' + " " + f_output.toFixed(2);
+
+    var c_output = c_starting.value;
+    var c_output = ((c_starting.value) * 1);
+
+    cOutput.innerHTML = 'Celsius:' + " " + c_output.toFixed(2);
+
+    var k_output = c_starting.value;
+    var k_output = ((c_starting.value) * 1 + 273.15);
+
+    kOutput.innerHTML = 'Kelvin:' + " " + k_output.toFixed(2);
+    
+
+
+}
+
+function convertNoK() {
+    var f_output = k_starting.value;
+    var f_output = (((k_starting.value) - 273.15) * 1.8 + 32);
+    fOutput.innerHTML = 'Fahrenheit:' + " " + f_output.toFixed(2);
+
+    var c_output = k_starting.value;
+    var c_output = ((k_starting.value) * 1 - 273.15);
+
+    cOutput.innerHTML = 'Celsius:' + " " + c_output.toFixed(2);
+
+    var k_output = k_starting.value;
+    var k_output = ((k_starting.value) * 1);
+
+    kOutput.innerHTML = 'Kelvin:' + " " + k_output.toFixed(2);
+    
+    
+}
+
+
+
+var footerInfo = document.getElementById('footer_info');
